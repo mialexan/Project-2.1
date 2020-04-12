4494041b5a934a1b9653a2180381a4bcfb0a9645
mialexan <62360903+mialexan@users.noreply.github.com>
2020-04-12 05:04:09 -0400

    Add files via upload

---
+++ Project 2_1.py
@@ -0,0 +1,14 @@
+#Import pyplot from matplotlib
+from matplotlib import pyplot as p
+
+#x-axis vaues
+x = [6, 4, 8, 2, 11, 9, 7]
+
+#y-axis values
+y = [3, 5, 7, 2, 8, 10, 12]
+
+#Takes the x and y values and places them in a scatter plot
+p.scatter(x,y)
+
+#Shows the scatter plot
+p.show()
